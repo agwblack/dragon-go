c02b56495303ec223db98ca08048ca964357f89d
Andrew Black <agwblack@gmail.com>
2012-02-23 13:10:04 +0000

    Updated classes

--- src/GameList.java
+++ src/GameList.java
@@ -18,8 +18,37 @@ public class GameList {
    */
   void update() {
     System.out.println("Updating GameList " + category);
+    /* The following is how to update the immediate games list, since that is
+     * most important and easiest to do first */
+
+    // getStatus();
+
+    // parseStatus(); to get current list
+
+    // compareList(); compare to previous list - might have new games, might 
+    // have fewer games (if we played a move), might have no change in some 
+    // games, might have the same games but a move further on (although this 
+    // should not happen if we update the list after playing a move).
+    
+    // Depending on the outcome of compareList(), we may need to addGame();
+    // removeGame(); updateGame(); moveGame() to a different list; copyGame() to a
+    // different list. These should be directly called from compareList()
   }
 
+  // Methods referenced from update() method
+  void retrieveStatus();
+  void parseStatus();
+  void compareList();
+  void addGame();
+  void removeGame();
+  void copyGame();
+  void moveGame();
+  void updateGame();
+
+  /*
+   * Method to return a string array containing the names of the opponents for
+   * each game in the list
+   */
   String[] getGameListString() {
     String[] list = new String[games.length];
     int i;
