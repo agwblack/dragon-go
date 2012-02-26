52b401c7913e544706a7cb8c96e1f1ad1fc8d0df
Andrew Black <agwblack@gmail.com>
2012-02-26 20:16:40 +0000

    Bringing in files from an initial play with UI, specifically the Login screen.

---
+++ src/com/agwblack/DragonGo/ListAct.java
@@ -0,0 +1,13 @@
+package com.agwblack.androidui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListAct extends Activity {
+  @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.listlayout);
+    }
+}
+
---
+++ src/com/agwblack/DragonGo/Login.java
@@ -0,0 +1,56 @@
+package com.agwblack.androidui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.util.Log;
+
+public class main extends Activity
+{
+    /** tag for logging purpose */
+    private static final String TAG = "***ANDROID_UI_MAIN***";
+
+    private TextView user;
+    private TextView passwd;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Log.d(TAG, "CREATED ACTIVITY MAIN");
+        user = (TextView) findViewById(R.id.username);
+        passwd = (TextView) findViewById(R.id.password);
+    }
+
+    /** Tries to login with the given details*/
+    //boolean login(CharSequence username, CharSequence password) {
+    boolean login(String username, String password) {
+      if (username.compareTo("agwblack") == 0 
+          && password.compareTo("sp00ner!") == 0) {
+        return true;
+      } else {
+        Log.d(TAG, "Username: " + username + "Password: " + password);
+        return false;
+      }
+    }
+
+    /** Called when we click the button specified in the layout main.xml*/
+    public void launchListActivity(View view) {
+      Log.d(TAG, "BUTTON PRESSED!!");
+      if (login(user.getText().toString(), passwd.getText().toString())) {
+        Intent intent = new Intent(main.this, ListAct.class);
+        startActivity(intent);
+      } else {
+        Toast failed =Toast.makeText(getApplicationContext(), "Bad username or password",
+          Toast.LENGTH_SHORT);
+        failed.show();
+      }
+    }
+
+}
